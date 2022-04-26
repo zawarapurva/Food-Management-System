dbf8d1ab3b9d56e874050b08b21388ae0375a2a7
SumedhaKasarla <kasarla.s@northeastern.edu>
2022-04-26 18:54:25 -0400

    libraries

--- Food-Management-/src/Business/Role/TransportRole.java
+++ Food-Management-/src/Business/Role/TransportRole.java
@@ -4,26 +4,25 @@
  */
 package Business.Role;
 
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Organization.DistributorOrganization;
+import Business.Organization.TransportOrganization;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import UserInterface.DistributorWorkAreaJPanel;
+import javax.swing.JPanel;
+
+
 /**
  *
  * @author kasar
  */
-
-import Business.EcoSystem;
-import Business.Enterprise.Enterprise;
-import Business.Organization.Organization;
-import Business.UserAccount.UserAccount;
-import UserInterface.AdminWorkAreaJPanel;
-import javax.swing.JPanel;
-
-
-    public class AdminRole extends Role {
+public class TransportRole extends Role {
     
-     @Override
+    @Override
     public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
-        return new AdminWorkAreaJPanel(userProcessContainer, enterprise);
+        return new DistributorWorkAreaJPanel(userProcessContainer, account, (DistributorOrganization)organization, enterprise, business);
     }
-    
-} {
     
 }
