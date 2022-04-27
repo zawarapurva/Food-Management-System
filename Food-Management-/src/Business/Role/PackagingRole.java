d27841f474b8c90e6481744f123b2042f40ceecd
SumedhaKasarla <kasarla.s@northeastern.edu>
2022-04-27 15:20:33 -0400

    Revert "New Roles"
    This reverts commit 4c4a8b5a1158cd48e5f332b8cbdcabc363547273.

--- Food-Management-/src/Business/Role/PackagingRole.java
+++ Food-Management-/src/Business/Role/PackagingRole.java
@@ -4,24 +4,10 @@
  */
 package Business.Role;
 
-import Business.EcoSystem;
-import Business.Enterprise.Enterprise;
-import Business.Organization.DistributorOrganization;
-import Business.Organization.PackagingOrganization;
-import Business.Organization.Organization;
-import Business.UserAccount.UserAccount;
-import UserInterface.DistributorWorkAreaJPanel;
-import javax.swing.JPanel;
-
 /**
  *
  * @author kasar
  */
-public class PackagingRole extends Role {
-    @Override
-    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, EcoSystem business) {
-        return new DistributorWorkAreaJPanel(userProcessContainer, account, (DistributorOrganization)organization, enterprise, business);
-    }
-    
+public class PackagingRole {
     
 }
